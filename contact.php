533e9289d7f03cfc0140b86acc357163e29cdb4a
freepickupkr <138541321+freepickupkr@users.noreply.github.com>
2023-07-05 13:30:42 +0900

    Add files via upload

--- contact.php
+++ contact.php
@@ -34,10 +34,29 @@
   <link rel="stylesheet" href="plugins/card-slider/css/style.css">
 
 
-  <!-- Main Stylesheet -->
-  <link href="css/style.css" rel="stylesheet">
   
+  <!--Favicon-->
+  <link rel="apple-touch-icon" sizes="57x57" href="images/fa/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="images/fa/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="images/fa/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="images/fa/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="images/fa/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="images/fa/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="images/fa/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="images/fa/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="images/fa/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="images/fa/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="images/fa/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="images/fa/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="images/fa/favicon-16x16.png">
+  <link rel="manifest" href="images/fa/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="images/fa/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="shortcut icon" href="images/fa/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="images/fa/favicon.ico" type="image/x-icon">
 
+<!-- Main Stylesheet -->
 <script language="JavaScript" src="genvalid.js" type="text/javascript"></script>
 </head>
 
