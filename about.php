8a958be81c04847410294b8fb224188784007e65
freepickupkr <138541321+freepickupkr@users.noreply.github.com>
2023-08-08 15:10:40 +0900

    Update about.php

--- about.php
+++ about.php
@@ -60,15 +60,17 @@
   <meta name="theme-color" content="#ffffff">
   <link rel="shortcut icon" href="images/fa/favicon.ico" type="image/x-icon">
   <link rel="icon" href="images/fa/favicon.ico" type="image/x-icon">
+
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-N3PJS4G40G"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9KDTZL6380"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-N3PJS4G40G');
+  gtag('config', 'G-9KDTZL6380');
 </script>
+  
 </head>
 
 <body>
